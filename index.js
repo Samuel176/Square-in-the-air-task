66402e959e4e49108a9331fe8e010506ba052dc3
sam elliott <73132951+Samuel176@users.noreply.github.com>
2025-08-11 14:31:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const countDownDate = new Date("Aug 20, 2025 10:00:00").getTime();
+// "Aug 20, 2025 10:00:00"
+const time = setInterval(function(){
+
+    let currentTime = new Date().getTime();
+
+    let timeDifferance = countDownDate - currentTime;
+
+    let days = Math.floor(timeDifferance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeDifferance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeDifferance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeDifferance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "D " + hours + "H "
+  + minutes + "M " + seconds + "S ";
+
+
+  if(timeDifferance <= 0 ){
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("play").style.display = "block";
+  }
+}, 100)
+
